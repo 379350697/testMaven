96eb3218f033c40aa052b85062a003921eda1dbe
wanglin <wanglin@88huicai.com>
2017-07-07 16:55:41 +0800

    test

--- src/main/java/com/design/observer/Observer.java
+++ src/main/java/com/design/observer/Observer.java
@@ -1,8 +1,20 @@
 package com.design.observer;
 
+
+import java.util.*;
+
 /**
  * @author wanglin
  * @date 2017/6/16
  */
 public interface Observer {
+
+	public AbstractList testInheritance(Map m);
 }
+
+class A implements  Observer{
+	public AbstractList testInheritance(HashMap m) {
+		return null;
+	}
+}
+
